6a3cd808d7376124fe6c8ad8fbbc8371cf6da8b3
Janghyeon <hwahyeon@knou.ac.kr>
2024-07-07 15:12:09 +0200

    refactor: Extract country code selection feature into separate function

--- burnout-test.lisp
+++ burnout-test.lisp
@@ -11,6 +11,18 @@
   "Prompt the user to enter their name and return it as a string. Default to 'Anonymous' if no input is provided."
   (read-input "Please enter your name: " "Anonymous"))
 
+(defun load-questions (country-code)
+  "Load the appropriate questions based on the country code."
+  (let* ((package-suffix (case (intern country-code :keyword)
+                           (:KR "kr")
+                           (:GR "gr")
+                           (:RS "rs")
+                           (:MY "my")
+                           (t "en")))
+         (questions-path (format nil "questions/~a.lisp" package-suffix)))
+    (load questions-path)
+    (intern (string-upcase (format nil "questions-~a" package-suffix)) :keyword)))
+
 (defun main ()
   "Main function for the burnout test program."
   (clear-screen)
@@ -19,31 +31,22 @@
     (clear-screen)
     (print-cbi-intro)
     (let* ((country-code (string-upcase (read-input "Please enter your country code: ")))
-           (package-suffix (case (intern country-code :keyword)
-                            ;  (:DE "de")
-                             (:KR "kr")
-                             (:GR "gr")
-                             (:RS "rs")
-                             (:MY "my")
-                             (t "en")))
-           (questions-package (intern (string-upcase (format nil "questions-~a" package-suffix)) :keyword)))
-      ;; Load the questions based on country code
-      (load (format nil "questions/~a.lisp" package-suffix))
+           (questions-package (load-questions country-code)))
       (format t "Personal Burnout Questions:~%")
-      (let ((personal-responses (ask-questions (funcall (intern (string-upcase "personal-burnout-questions") questions-package)))))
+      (let ((personal-responses (ask-questions (funcall (intern "PERSONAL-BURNOUT-QUESTIONS" questions-package)))))
         (format t "~&Work-related Burnout Questions:~%")
-        (let ((work-responses (ask-questions (funcall (intern (string-upcase "work-related-burnout-questions") questions-package)))))
+        (let ((work-responses (ask-questions (funcall (intern "WORK-RELATED-BURNOUT-QUESTIONS" questions-package)))))
           (format t "~&Client-related Burnout Questions:~%")
-          (let ((client-responses (ask-questions (funcall (intern (string-upcase "client-related-burnout-questions") questions-package)))))
+          (let ((client-responses (ask-questions (funcall (intern "CLIENT-RELATED-BURNOUT-QUESTIONS" questions-package)))))
             ;; Calculate average scores for each category
             (clear-screen)
             (let* ((personal-average (calculate-average personal-responses))
                    (work-average (calculate-average work-responses))
                    (client-average (calculate-average client-responses)))
-            (print-test-results personal-average work-average client-average name)
-            (format t "Press any key to exit...")
-            (finish-output)
-            (read-line))))))))
+              (print-test-results personal-average work-average client-average name)
+              (format t "Press any key to exit...")
+              (finish-output)
+              (read-line))))))))
 
 ;; Code to create the executable
 (sb-ext:save-lisp-and-die "burnout-test.exe"
