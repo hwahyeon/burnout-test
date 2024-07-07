6b377c699ceb701c4e78118ac6ba29aaad449ba4
Janghyeon <hwahyeon@knou.ac.kr>
2024-07-07 15:28:32 +0200

    refactor: Extract ask-questions feature into separate function

--- burnout-test.lisp
+++ burnout-test.lisp
@@ -23,6 +23,16 @@
     (load questions-path)
     (intern (string-upcase (format nil "questions-~a" package-suffix)) :keyword)))
 
+(defun ask-all-questions (questions-package)
+  "Ask all categories of questions and return the responses."
+  (format t "Personal Burnout Questions:~%")
+  (let ((personal-responses (ask-questions (funcall (intern "PERSONAL-BURNOUT-QUESTIONS" questions-package)))))
+    (format t "~&Work-related Burnout Questions:~%")
+    (let ((work-responses (ask-questions (funcall (intern "WORK-RELATED-BURNOUT-QUESTIONS" questions-package)))))
+      (format t "~&Client-related Burnout Questions:~%")
+      (let ((client-responses (ask-questions (funcall (intern "CLIENT-RELATED-BURNOUT-QUESTIONS" questions-package)))))
+        (values personal-responses work-responses client-responses)))))
+
 (defun main ()
   "Main function for the burnout test program."
   (clear-screen)
@@ -32,19 +42,15 @@
     (print-cbi-intro)
     (let* ((country-code (string-upcase (read-input "Please enter your country code: ")))
            (questions-package (load-questions country-code)))
-      (format t "Personal Burnout Questions:~%")
-      (let ((personal-responses (ask-questions (funcall (intern "PERSONAL-BURNOUT-QUESTIONS" questions-package)))))
-        (format t "~&Work-related Burnout Questions:~%")
-        (let ((work-responses (ask-questions (funcall (intern "WORK-RELATED-BURNOUT-QUESTIONS" questions-package)))))
-          (format t "~&Client-related Burnout Questions:~%")
-          (let ((client-responses (ask-questions (funcall (intern "CLIENT-RELATED-BURNOUT-QUESTIONS" questions-package)))))
-            ;; Calculate average scores for each category
-            (clear-screen)
-            (let* ((personal-average (calculate-average personal-responses))
-                   (work-average (calculate-average work-responses))
-                   (client-average (calculate-average client-responses)))
-              (print-test-results personal-average work-average client-average name)
-              (press-any-key-to-exit))))))))
+      (multiple-value-bind (personal-responses work-responses client-responses)
+          (ask-all-questions questions-package)
+        ;; Calculate average scores for each category
+        (clear-screen)
+        (let* ((personal-average (calculate-average personal-responses))
+               (work-average (calculate-average work-responses))
+               (client-average (calculate-average client-responses)))
+          (print-test-results personal-average work-average client-average name)
+          (press-any-key-to-exit))))))
 
 ;; Code to create the executable
 (sb-ext:save-lisp-and-die "burnout-test.exe"
